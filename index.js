b9422a40d71e7c920cb606fd1e9d3138be4d3682
Danny Fuhriman <danny.fuhriman@Okta.com>
2022-02-09 11:54:33 -0800

    cleanup code

--- index.js
+++ index.js
@@ -7,39 +7,60 @@ const getHash = async (state) => {
     try {
         const { data, error } = await hget(state, 'data');
 
-        return { data, error };
-    } catch (e) {
-        console.error(e);
-        return e;
+        if (error) {
+            throw error;
+        }
+
+        return data;
+    } catch (error) {
+        console.error(
+            typeof error === 'string'
+                ? new Error(`Unable to get hash [${error}]`)
+                : error
+        );
+        throw error;
     }
 };
 
 const parseHeader = async (_data) => {
-    let result = {};
+    try {
+        let result = {};
+        _data &&
+            _data.split(';').forEach((item) => {
+                const parsedItem = item.split('=');
 
-    _data.split(';').forEach(item => {
-        const parsedItem = item.split('=');
+                const key = parsedItem[0];
+                const value = parsedItem[1];
 
-        const key = parsedItem[0];
-        const value = parsedItem[1];
+                if (value && value.includes('|')) {
+                    let nestedValue = {};
 
-        if (value.includes('|')) {
-            let nestedValue = {};
+                    value.split('|').forEach((item) => {
+                        const _parsedItem = item.split(':');
 
-            values.split('|').forEach(item => {
-                const _parsedItem = item.split(':');
-
-                nestedValue[_parsedItem[0]] = _parsedItem[1];
+                        nestedValue[_parsedItem[0]] = _parsedItem[1];
+                    });
+                    result[key] = nestedValue;
+                } else {
+                    result[key] = value;
+                }
             });
-            result[key] = nestedValue;
-        } else {
-            result[key] = value;
-        }
-    });
 
-    return result;
-}
-
+        return result;
+    } catch (error) {
+        console.error(
+            typeof error === 'string'
+                ? new Error(`Unable to parse header [${error}]`)
+                : error
+        );
+        throw error;
+    }
+};
+/*
+For event hooks, Okta does a GET call to perform verification.
+This is not done for token hooks but code is provided so this
+worker can also be used for an event hook.
+*/
 const verify = (req, res) => {
     try {
         const challenge = req.headers['x-okta-verification-challenge'];
@@ -47,12 +68,15 @@ const verify = (req, res) => {
         let resp = { verification: challenge };
 
         return new Response(JSON.stringify(resp));
-
     } catch (error) {
-        throw error
+        console.error(
+            typeof error === 'string'
+                ? new Error(`Unable to handle Okta verification [${error}]`)
+                : error
+        );
+        throw error;
     }
-}
-
+};
 
 /*
 Create a new router
@@ -68,39 +92,52 @@ router.get('*', (req, res) => verify(req, res));
 Handle an Okta token hook request
 */
 router.POST('*', async (req, res) => {
-    const { authorization } = Object.fromEntries(req.headers) || {};
+    try {
+        const { authorization } = Object.fromEntries(req.headers) || {};
 
-    if (!authorization || authorization !== API_KEY) {
-        return new Response('Unauthorized', { status: 401 });
-    }
-
-    const { context, identity } = req.body.data || {};
-    const { user, protocol: { request } } = context || {};
-    const { state } = request || {};
-
-    const akamaiHeader = await parseHeader(await getHash(state)) || undefined;
-
-    if (akamaiHeader) {
-        const response = {
-            commands: [
-                {
-                    type: 'com.okta.access.patch',
-                    value: [
-                        {
-                            op: 'add',
-                            path: '/claims/risk_profile',
-                            value: akamaiHeader
-                        }
-                    ]
-                }
-            ]
+        if (!authorization || authorization !== API_KEY) {
+            return new Response('Unauthorized', { status: 401 });
         }
 
-        return new Response(JSON.stringify(response));
-    } else {
-        return new Response(null, { status: 204 });
-    }
+        const { data } = (await req.json()) || {};
+        const {
+            context: { protocol },
+        } = data || {};
+        const {
+            request: { state },
+        } = protocol || {};
 
+        const akamaiHeader =
+            (await parseHeader(await getHash(state))) || 'uh=oh';
+
+        if (akamaiHeader) {
+            const response = {
+                commands: [
+                    {
+                        type: 'com.okta.access.patch',
+                        value: [
+                            {
+                                op: 'add',
+                                path: '/claims/risk_profile',
+                                value: akamaiHeader,
+                            },
+                        ],
+                    },
+                ],
+            };
+
+            return new Response(JSON.stringify(response));
+        } else {
+            return new Response(null, { status: 204 });
+        }
+    } catch (error) {
+        console.error(
+            typeof error === 'string'
+                ? new Error(`Unable to handle POST request [${error}]`)
+                : error
+        );
+        throw error;
+    }
 });
 
 /*
